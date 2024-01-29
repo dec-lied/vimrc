7641dd52bd4a5c14ccb67be85895da87ce208dfd
dec-lied <gavindhondt@hotmail.com>
2024-01-29 11:35:15 -0500

    added neo-tree to neovide

--- lua/plugins/misc/neovide.lua
+++ lua/plugins/misc/neovide.lua
@@ -7,12 +7,24 @@ if vim.g.neovide then
     vim.g.neovide_cursor_animation_length = 0
 
     return {
-        "nvimdev/dashboard-nvim",
-        event = "VimEnter",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("dashboard").setup {}
-        end
+        {
+            "nvimdev/dashboard-nvim",
+            event = "VimEnter",
+            dependencies = { "nvim-tree/nvim-web-devicons" },
+            config = function()
+                require("dashboard").setup {}
+            end
+        },
+        {
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v3.x",
+            dependencies = {
+              "nvim-lua/plenary.nvim",
+              "nvim-tree/nvim-web-devicons",
+              "MunifTanjim/nui.nvim",
+              -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+            }
+        }
     }
 else
     return {}
